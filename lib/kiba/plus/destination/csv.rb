98bc614d47418d1938a7d5adc3b87e063e640966
Hooopo <hoooopo@gmail.com>
2016-05-18 03:16:26 +0800

    csv to pg example

--- lib/kiba/plus/destination/csv.rb
+++ lib/kiba/plus/destination/csv.rb
@@ -6,16 +6,44 @@ module Kiba::Plus::Destination
 
     def initialize(options = {})
       @options = options
-      @options.assert_valid_keys(:output_file)
-      @csv = CSV.open(output_file, 'w', {col_sep: delimiter})
+      @options.assert_valid_keys(
+        :output_file,
+        :row_sep,
+        :col_sep,
+        :force_quotes,
+        :quote_char,
+        :mode
+      )
+      @csv = CSV.open(output_file, mode, {
+        :col_sep => col_sep,
+        :quote_char => quote_char,
+        :force_quotes => force_quotes,
+        :row_sep => row_sep
+      })
+    end
+
+    def mode
+      options.fetch(:mode, "w")
     end
 
     def output_file
       options.fetch(:output_file)
     end
 
-    def delimiter
-      ","
+    def col_sep
+      options.fetch(:col_sep, ",")
+    end
+
+    def quote_char
+      options.fetch(:quote_char, '"')
+    end
+
+    def force_quotes
+      options.fetch(:force_quotes, false)
+    end
+
+    def row_sep
+      options.fetch(:row_sep, "\n")
     end
 
     def write(row)
